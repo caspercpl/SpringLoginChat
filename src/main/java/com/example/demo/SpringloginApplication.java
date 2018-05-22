06e94971495e9c4a2ebf74fca35843628427dbaa
caspercpl <caspercpl@gmail.com>
2018-05-22 09:41:01 +0200

    uploade files

---
+++ src/main/java/com/example/demo/SpringloginApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringloginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringloginApplication.class, args);
+	}
+}
